d66268981a2ea45ce0e7e97a23058a438c225f14
shotcaller <ruturaj.ghodke77@gmail.com>
2024-01-02 02:47:36 +0530

    setup firebase firestore

---
+++ src/config/livefeed-edea6-firebase-adminsdk-ih3mi-c292780a9f.js
@@ -0,0 +1,13 @@
+export const serviceAccount = {
+  type: "service_account",
+  project_id: "livefeed-edea6",
+  private_key_id: "c292780a9f7162a2ccdcba68714706c0a5abb69b",
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC+agvtl9GZQ6/h\nIPV0aZsVn+I2P5K8fJWSW8IzRfXCVKSEaiW+F9b0eRagb3Y+QtcQVMW98ZzP7Aj/\nu8Dgd9pnTsSy+h4a7oUL25G0EGwBquVue/5LO6kIAfvuLXGtUKgx7X5RfLKahndW\nFCMbyZKWgkBH8HS1YFKFDr5VD9c5pcLVpX284N+LF+tLsHpPQjOWq3NDa3ZHsyIE\nEfRoEJx/rXegQdNuEDm3uZ8WHrO9vnwsNSF6Rych35ib3w2ucgiwfA8UZo7R3saD\nSunYx8aHRsDz0XwPEwHlvtzdj7/Jy7C1hO/yhqQWndsv+YCnEOLjJEE67rBz19C+\nRwf94GNrAgMBAAECggEAHTFF7eGiNTSe4M18RCIysBCIjleVyAbzqXIe5lx57paq\neFCLw8aE4osz8FnWT3pQ3YSZomLJ70iZIpfm3KMTXVZCqsvU1L0vsZ8yieAgpurL\n7PHejSsy4XLobF4UOQWOG8V04cpT2YP3BL/mmAqBykhi0YSoZ+xCCns1q/XXQdOH\nubXTopOF7yI3VWk1KmpKTZDmrV2bYA39Tc4kElCrKxCbNKpo6G4PzV0m29p7F5Pt\nPkh61+eZqQU65mLtZKTUIRb21xeKfGhCrmU7nf3w+EdCXTyxt1iVC3jmc85DfWkx\nHOlxUVQbhIUYQPJQuIEuqF6GWYjvBX3BQBkTRxeXdQKBgQDlSTK2vFNsXsKPpFWm\nSLxSsR3eayEIOPefdiOxW3Wd6l98zmafDoP7kGpm1Cqq+1VlWhg1ftS1+bdyNaa7\n1D72WMAn/QwAS3NdUaO1bVsTLmWlpxJG5PiD76hu9315KNne2ga76keGXWFmzJr3\nifu+63YjVt/YG0MxigkPcSNMnwKBgQDUmXD457tjYoyCEqIKPdoHreodZ8f1bFh9\nUt8xuYMsC2SFkkH/ZM9cebWqQBwwwomUUyiGNZqvNTRyFOVAS6bXVeJnOTsm5AKj\nnP+Ng408+qBzOlL5Y8fAeQaFBADxUhUiDMFSXupJq+unYh56KXsG20DyTrsyOZqT\n666xa8JptQKBgDJJ2aumpo1wQzAKFRcBgPyKCEkOKhKJnBYHbVCrBKL8ErFm+68L\nGfuE8zFWe31A3rv6BLFG/hRjjs6n4cf0h1K0qICcyZMO+WCQVV2nfIA6fuTUPff6\npF8bo0+jTIvUX+S24R86BTH9LS3E1oBQbTy0nBaoKTRW1mf5JH+Uiu91AoGBAJGe\nO/Qw/XgYczdQve6X6PQjmlzjzeF7gazti3UKpR8Q+pxGzq+5BBNKRjVDzktTpFqV\nDwfGRZpGrhKJ33DHoJKUAAxF9g8wU+JvUddDSQWAgVALnfO19YcPHnkTgfXJDDKT\nXAMogf9OI441tbxlFyBeI0EUzkGblRZGcxSRQ8uVAoGAARF6iC3BmMtfvgLyThI0\ng69pIHim+tNpSZcGDISvlbALrxTBVtls+PQnroZdtFe3rvs9f6BfnKGvxA+xFUdq\nO2W9xe5OjwwPvUtZjhTk+qowHlh4Hi0KobeOmOtiBrPPmolsnw84EEIm6fRnyOUc\nki9V+GN7WFyK5KgrqvrO0A0=\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-ih3mi@livefeed-edea6.iam.gserviceaccount.com",
+  client_id: "109120846779688736663",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ih3mi%40livefeed-edea6.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+}
